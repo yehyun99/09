a2f9b7dcc360bc3b387031ad0daf1cf390a66917
이예현 <shszyh@iclooud.com>
2018-11-22 16:41:38 +0900

    06

--- 09.cpp
+++ 09.cpp
@@ -1,32 +1,30 @@
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	
-	FILE*fp, *fp2;
-	char input;
-	int i;
-	char original[100];
-	char copy[100];
-	
-	printf("original file: ");
-	scanf("%s",original);
-	printf("copy file: ");
-	scanf("%s",copy);
-	scanf("%s",original);
-	
-	fp=fopen(original,"r");
-	fp2=fopen(copy,"w");
-	
-	while((input=fgetc(fp))!=EOF)
-	{
-		fputc(input,fp2);
-		i++;
-	}
-	printf("copy succeed~(%i bytes copied)\n",i);
-	fclose(fp);
-	fclose(fp2);
+	FILE *fp;
+	char word[100];
+	char filename[100];
+	char input[100];
 
+	printf("original file name: ");
+	scanf("%s",filename);
+	
+	fp=fopen(filename,"r");
+	
+	printf("word to find: ");
+	scanf("%s",word);
+
+	while(fgets(input,100,fp)!=NULL)
+	{
+		if (strncmp(input,word,strlen(word))==0)
+		{
+			printf("fine a word %s\n", input);
+		}
+	}
+	printf("search done!\n");
 	return 0;
 }
